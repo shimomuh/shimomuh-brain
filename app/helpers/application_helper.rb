169844ea4a1cecd93e801647853f2b94662c973c
hiroshi-shimomura <hiroshi.shimomura@dena.com>
2017-07-26 20:35:35 +0900

    add helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  include ActionView::Helpers::AssetTagHelper
+
+  # FIXME: That's too bad...
+  def custom_image_url(url)
+    image_url(url).gsub!('http://0.0.0.0:3000', 'http://shimomuh-brain.net')
+  end
 end
